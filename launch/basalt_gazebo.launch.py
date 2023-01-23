6536473634e570a1bfe99e3383c401f11f5232f0
rdeb36342 <rens.de.boer@demcon.com>
2023-01-23 10:18:21 +0100

    Add launch/config for Gazebo simulation

---
+++ launch/basalt_gazebo.launch.py
@@ -0,0 +1,40 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+from os.path import expanduser
+
+def generate_launch_description():
+      
+    cal_file = expanduser('~/bas_repo/src/basalt_ros/basalt_ros2/config/gazebo_calib.json')
+    vio_file = expanduser('~/bas_repo/src/basalt_ros/basalt_ros2/config/gazebo_vio.json')    
+        
+    return launch.LaunchDescription([
+            launch_ros.actions.Node(
+                package='basalt_ros2',
+                executable='basalt_vio_node',
+                name='basalt_vio',
+                output='screen',
+                #prefix=['gdbserver localhost:3000'],
+                parameters=[{
+                         'calibration_file': cal_file,
+                         'vio_config_file': vio_file,
+                         'debug_vio': False,
+                         'debug_bad_data': False,
+	                     # 'extra_translation': [0.0, 0.0, 0.0],
+                         # rotation is in format [w, x, y, z]
+	                     # 'extra_rotation': [0.0, 0.0, 0.0, 1.0],
+                }],
+                remappings=[('/left_image', '/camera/infra1/image_raw'),
+                        ('/right_image', '/camera/infra2/image_raw'),
+                        ('/imu_topics', '/iris/imu'),
+                        ('/odom', '/basalt_vio/odom')
+                 ]
+                # remappings=[('/left_image', '/dora_cam/left/image_raw'),
+                #         ('/right_image', '/dora_cam/right/image_raw'),
+                #         ('/gyro', '/dora/imu'),
+                #         ('/accel', '/dora/imu'),
+                #         ('/odom', '/basalt_vio/odom')
+                #  ]                 
+        )    
+    ])
